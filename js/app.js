3e35a26e20c7fdef5795ba7eb37b11491b1bc368
RANA469 <ranashelbaya.98@yahoo.com>
2021-01-23 22:37:30 +0200

    fix the local storage

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@ var thirdimg = document.getElementById('third-image');
 var maxClickUser = 25;
 var userClickCounter = 0;
 
-chart();
+
 
 // constructer
 function prodimg(name, source) {
@@ -35,29 +35,10 @@ function prodimg(name, source) {
     value.push(name);
     allprodimgs.push(this);
 
-    indexImg = allprodimgs.indexOf(this.name);
-    imgName.push(name);
-}
-// prodimg.prototype.allprodimgs.push(this);
-
-
-var setmaxuserRoundsForm = document.getElementById('setmaxuserRounds')
-setmaxuserRoundsForm.addEventListener('submit', submit);
-
-function submit(event) {
-    console.log(event);
-    event.preventDefault();
-    maxClickUser = event.target.rounds.value;
+    indexProd = allprodimgs.indexOf(this.name);
+    name.push(name);
 }
 
-imagesDiv.addEventListener('click', clickByUser);
-resultslist.addEventListener('click', resultslist);
-setmaxuserRounds.addEventListener('click', setmaxuserRounds);
-resultslistbutton.addEventListener('click', seeResults);
-
-
-
-prodimg.prototype.allprodimgs = [];
 new prodimg('bag', '../img/bag.jpg');
 new prodimg('banana', '../img/banana.jpg');
 new prodimg('bathroom', '../img/bathroom.jpg');
@@ -83,51 +64,6 @@ new prodimg('wine-glass', '../img/wine-glass.jpg');
 var previousIndex = [-1, -1, -1];
 var previous;
 
-
-// to make random number for the img 
-function randomNumbGenerator() {
-    return Math.floor(Math.random() * (prodimg.prototype.allprodimgs.length));
-}
-randomNumbGenerator();
-
-
-
-// the chart 
-function chart() {
-    var votes = [];
-    for (var i = 0; i < prodimg.prototype.allprodimgs.length; i++) {
-        votes.push(prodimg.prototype.allprodimgs[i].vote);
-        console.log(prodimg.prototype.allprodimgs[i].vote);
-
-    }
-    console.log(prodimg.prototype.allprodimgs);
-
-    var ctx = document.getElementById('myChart').getContext('2d');
-    var chart = new Chart(ctx, {
-        // The type of chart we want to create
-        type: 'bar',
-        // The data for our dataset
-        data: {
-            labels: ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duch', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'],
-            datasets: [{
-                label: 'My First dataset',
-                backgroundColor: 'rgb(255, 99, 132)',
-                borderColor: 'rgb(255, 99, 132)',
-                data: votes
-            }]
-        },
-        // Configuration options go here
-        options: {}
-    });
-}
-
-form.removeEventListener('submit', clickByUser)
-var firstimage = document.getElementById('clickByUser');
-var secondimage = document.getElementById('clickByUser');
-var thirdimage = document.getElementById('clickByUser');
-
-console.log(firstimg, secondimg, thirdimg)
-
 function renderThreeRandomImages() {
     do {
         previous = false;
@@ -152,12 +88,166 @@ function renderThreeRandomImages() {
     check[0] = firstIndex; check[1] = secondIndex; check[2] = thirdIndex;
     // added the img (objects)
     firstIndex.src = allImg[firstIndex].source;
-    allImg[firstIndex].views++;
+    allprodimgs[firstIndex].views++;
     middleImg.src = allImg[secondIndex].source;
     allprodimgs[secondIndex].views++;
     rightImg.src = allprodimgs[thirdIndex].source;
     allprodimgs[thirdIndex].views++;
 }
+
+// to make random number for the img 
+function randomNumbGenerator() {
+    return Math.floor(Math.random() * (prodimg.prototype.allprodimgs.length));
+}
+randomNumbGenerator();
+
+var setmaxuserRoundsForm = document.getElementById('setmaxuserRounds')
+setmaxuserRoundsForm.addEventListener('submit', submit);
+
+function submit(event) {
+    console.log(event);
+    event.preventDefault();
+    maxClickUser = event.target.rounds.value;
+}
+setmaxuserRoundsForm.removeEventListener('submit', clickByUser)
+
+var firstimage = document.getElementById('clickByUser');
+var secondimage = document.getElementById('clickByUser');
+var thirdimage = document.getElementById('clickByUser');
+
+
+
+
+function clickByUser(event) {
+    console.log('clickByUser');
+    if (userClickCounter < maxClickUser) {
+        if (event.target.id === 'first-image') {
+         allprodimgs[firstIndex].votes++;
+
+        } else if (event.target.id === 'second-image') {
+           allprodimgs[secondIndex].votes++;
+        }
+     else {
+        allprodimgs[thirdimage].votes++;
+        //   (event.target.id === 'third-image') 
+    }
+    userClickCounter++;
+    renderThreeRandomImages();
+}
+if ( userClickCounter == maxClickUser) {
+    // ending 
+    document.getElementById('show-result').style.visibility = "visible";
+    var viewResult = document.getElementById('theResult');
+    viewResult.addEventListener('submit', theResult);
+    // remove events
+    firstimage.removeEventListener('click', clickByUser);
+    secondimage.removeEventListener('click', clickByUser);
+    thirdimage.removeEventListener('click', clickByUser);
+}
+}
+function seeResults(event) {
+    event.preventDefault();
+    for (var i = 0; i < allprodimgs.length; i++) {
+        perVote.push(allprodimgs[i].votes);
+        perViow.push(allprodimgs[i].views);
+    }
+
+// the chart
+    function chart() {
+        var votes = [];
+        for (var i = 0; i < prodimg.prototype.allprodimgs.length; i++) {
+            votes.push(prodimg.prototype.allprodimgs[i].vote);
+            console.log(prodimg.prototype.allprodimgs[i].vote);
+    
+        }
+        console.log(prodimg.prototype.allprodimgs);
+    
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var chart = new Chart(ctx, {
+            // The type of chart we want to create
+            type: 'bar',
+            // The data for our dataset
+            data: {
+                labels: ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duch', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'],
+                datasets: [{
+                    label: 'My First dataset',
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: votes
+                }]
+            },
+            // Configuration options go here
+            options: {}
+        });
+    }
+
+    chart();
+    
+
+
+
+
+
+// imagesDiv.addEventListener('click', clickByUser);
+// resultslist.addEventListener('click', resultslist);
+// setmaxuserRounds.addEventListener('click', setmaxuserRounds);
+// resultslistbutton.addEventListener('click', seeResults);
+// prodimg.prototype.allprodimgs = [];
+
+
+// the chart 
+
+// form.removeEventListener('submit', clickByUser)
+
+// console.log(firstimg, secondimg, thirdimg)
+
+
+
+
+
+
+
+
+// seeResults();
+// chart();
+// list();
+//     }
+
+
+
+
+
+// here is the storage data 
+// var setlist = [];
+// function list() {
+//     setlist = JSON.stringify(prodimg.prototype.allprodimgs);
+//     localStorage.setItem('setlist', setlist);
+
+// }
+
+// function getinfo() {
+//     var imgeslist = localStorage.getItem('allprodimgs');
+//     var list = JSON.parse(imgeslist);
+
+//     if (list !== null) {
+//         prodimg.prototype.allprodimgs = list;
+//         setlist = imgjs;
+//     }
+// }
+
+
+   // getinfo();
+    // var endResult;
+
+    // for (var i = 0; i < allprodimgs.length; i++) {
+    //     endResult = document.createElement('li');
+        // endResult.textContent = 'name';
+        // perVote.push(allprodimgs[i].votes);
+        // perViow.push(allprodimgs[i].view);
+        // console.log('prodimg');
+
+//     }
+// }
 // previousthirdIndex = previousthirdIndex
 
 
@@ -177,94 +267,7 @@ function renderThreeRandomImages() {
 // }
 
 
-renderThreeRandomImages();
+// renderThreeRandomImages();
 // firstimg.addEventListener()
-
-
-
-function clickByUser(event) {
-    console.log('clickByUser');
-    if (userClickCounter < maxClickUser) {
-        if (event.target.id === 'first-image') {
-            prodimg.prototype.allprodimgs[firstIndex].votes++;
-
-        } else if (event.target.id === 'second-image') {
-            prodimg.prototype.allprodimgs[secondIndex].votes++;
-        }
-
-    } else if (event.target.id === 'third-image') {
-    }
-    userClickCounter++;
-    renderThreeRandomImages();
 }
-
-
-
-if ( userClickCounter == maxClickUser) {
-    // handle end of voting
-    document.getElementById('show-result').style.visibility = "visible";
-    var viewResult = document.getElementById('theResult');
-    viewResult.addEventListener('submit', theResult);
-    // remove events
-    firstimage.removeEventListener('click', clickByUser);
-    secondimage.removeEventListener('click', clickByUser);
-    thirdimage.removeEventListener('click', clickByUser);
-}
-
-
-
-// seeResults();
-// chart();
-// list();
-//     }
-
-
-function seeResults(event) {
-    event.preventDefault();
-
-
-    for (var i = 0; i < allImg.length; i++) {
-        imgVote.push(allImg[i].votes);
-        imgView.push(allImg[i].views);
-    }
-    getinfo();
-    var endResult;
-
-    for (var i = 0; i < prodimg.prototype.allprodimgs.length; i++) {
-        endResult = document.createElement('li');
-        // endResult.textContent = 'name';
-        endResult.textContent = prodimg.prototype.allprodimgs[i].name + 'has'
-            + prodimg.prototype.allprodimgs[i].vote + ' votes , and was seen ' +
-            prodimg.prototype.allprodimgs[i].view + 'times.' + ' The percentage of selcting this  prodimg is :' + (prodimg.prototype.allprodimgs[i].
-                vote * 100 / prodimg.prototype.allprodimgs[i].view) + '%';
-        resultslist.appendChild(endResult);
-        // console.log('prodimg');
-
-    }
-}
-
-
-function percentage() {
-    for (var i = 0; i < allImg.length; i++) {
-        allImg[i].perViow = (allImg[i].shown * 100) / maxAttempts;
-        allImg[i].perVotes = (allImg[i].votes * 100) / maxAttempts;
-    }
-}
-
-// here is the storage data 
-var setlist = [];
-function list() {
-    setlist = JSON.stringify(prodimg.prototype.allprodimgs);
-    localStorage.setItem('setlist', setlist);
-
-}
-
-function getinfo() {
-    var imgeslist = localStorage.getItem('allprodimgs');
-    var list = JSON.parse(imgeslist);
-
-    if (list !== null) {
-        prodimg.prototype.allprodimgs = list;
-        setlist = imgjs;
-    }
 }
